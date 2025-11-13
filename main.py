ef5efecde8414943b4e75bfc7f84bc3875856ba9
yineth damarys castillo vergara <damarys>
2025-11-13 09:40:25 -0500

    se creo un menu donde salian las opciones de area de triangulo y cuadrado, y se agrego la suma de dos numeros

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 from funtions import suma
+from calculo import calcular_area_cU, calcular_area_t
 
-resultado =suma (3,5)
-print("hola mundo")
-print(resultado)
+def mostrar_menu():
+    
+    print("-----menu-----")
+    print("1 para calcular el area de triangulo")
+    print("2 para calcular el area del cuadrado")
+    print("3 para sumar")
+    print("4 para salir")
+
+while True:
+    mostrar_menu()
+    opcion=input("selecciona la opcion:")
+
+    if opcion == "1" :
+        
+        base= float(input("digite una base:"))
+        altura=float(input("digite una altura:"))
+        area1= calcular_area_t(base, altura)
+        print(f"el area del traingulo es: {area1}")
+
+
+    elif opcion == "2" :
+
+        lado=float(input("digite un lado:"))    
+        area2= calcular_area_cU (lado)
+        print(f"el area del cuadrado es: {area2}")
+
+    elif opcion == "3":
+
+        num1= float (input("digite un numero:"))
+        num2 =float (input("digite un numero:"))
+        resultado = suma (num1, num2) 
+        print(f"la suma es: {resultado}")
+
+    elif opcion == "4" :
+
+        print("hasta pronto!")
+        break
+    else:
+        print("opcion invalida, intente de nuevo")
+
